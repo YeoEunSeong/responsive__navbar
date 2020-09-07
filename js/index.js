3cfb86b60a8f1a7f96fb90beb9f8a1dd93752e70
Yeo, Eunseong <e7838752@gmail.com>
2020-09-08 03:39:45 +0900

    resposive__navbar

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector(".navbar__toggleBtn");
+const navbar__menu = document.querySelector(".navbar__menu");
+const navbar__icons = document.querySelector(".navbar__icons");
+
+toggleBtn.addEventListener('click', ()=>{
+    navbar__menu.classList.toggle('hidden');
+    navbar__icons.classList.toggle('hidden');
+})
